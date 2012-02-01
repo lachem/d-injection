87af7eb323ae769d088a7d4e38e7c86fa58fcf2d
sal-vage <sal-vage@64c13286-791c-4059-bc77-4e3ab86eb2f2>
2012-02-01 20:33:15 +0000

    REFERENCE: DEVELOPMENT STABLE: YES (SINGLE THREAD) TESTED: PARTIALLY (UT) DESCRIPTION: File cleanup

--- trunk/src/injectable.hpp
+++ trunk/src/injectable.hpp
@@ -13,6 +13,10 @@
 
 namespace di {
 
+#ifndef MAX_NUM_INJECTIONS
+#define MAX_NUM_INJECTIONS 10
+#endif
+
 #define BOOST_PP_LOCAL_LIMITS (1, MAX_NUM_INJECTIONS)
 #include BOOST_PP_LOCAL_ITERATE()
 
