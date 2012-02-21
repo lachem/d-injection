7522061e9bba2ff5f8024c0ab2820c0bc724bd3e
sal-vage <sal-vage@64c13286-791c-4059-bc77-4e3ab86eb2f2>
2012-02-21 21:28:54 +0000

    REFERENCE   : Development STABLE      : Yes TESTED      : Partially (UT) DESCRIPTION : a bit of simplification

--- di/detail/inject_container.hpp
+++ di/detail/inject_container.hpp
@@ -24,7 +24,7 @@ class inject_container {
 
 		inline bool is_in_range(char* address, size_t range) {
 			char* inject_address = reinterpret_cast<char*>(injection);
-			return (inject_address >= address && inject_address < address + range);
+			return (address <= inject_address && inject_address < address + range);
 		}
 
 		void* operator new(size_t size) {
@@ -46,10 +46,10 @@ public:
 	inline static void insert(T** injection) {
 		detail::lock_guard<detail::spin_lock> guard(lock);
 		if(0 == head) {
-			tail = head = new node();
+			tail = (head = new node());
 		}
 		else {
-			tail = tail->next = new node();
+			tail = (tail->next = new node());
 		}
 		tail->injection = injection;
 	}
@@ -64,17 +64,16 @@ public:
 				delete tmp;
 				return injection;
 			}
-			node* current = head;
-			while(0 != current->next) {
-				node* next = current->next;
-				if(next->is_in_range(address,range)) {
-					T** injection = next->injection;
-					node* tmp = next;
-					current->next = next->next;
-					delete next;
+			node* prev = head;
+			node* curr = head->next;
+			while(curr) {
+				if(curr->is_in_range(address,range)) {
+					T** injection = curr->injection;
+					prev->next = curr->next;
+					delete curr;
 					return injection;
 				}
-				current = next;
+				curr = curr->next;
 			}
 		}
 		return 0;
