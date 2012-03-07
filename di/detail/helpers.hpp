ee3e9060c2db3ebe42372f29bbc87519a1a42217
sal-vage <sal-vage@64c13286-791c-4059-bc77-4e3ab86eb2f2>
2012-03-07 22:21:21 +0000

    REFERENCE   : Development STABLE      : Yes TESTED      : Yes DESCRIPTION : Added assertions for successful use and replace

--- di/detail/helpers.hpp
+++ di/detail/helpers.hpp
@@ -22,9 +22,27 @@ struct set_null {
 };
 
 template<typename T>
+struct contains {
+	contains(T* an_object, bool* a_result) : object(an_object), result(a_result) {}
+	template<typename V>
+	void operator()(V& v, typename boost::enable_if<boost::is_same<V,T* > >::type* dummy = 0) const {
+		if(v == object) {
+			*result = true;
+		}
+	}
+	template<typename V>
+	void operator()(V& v,typename boost::disable_if<boost::is_same<V,T* > >::type* dummy = 0) const {
+		//empty
+	}
+private:
+	mutable T* object;
+	mutable bool* result;
+};
+
+template<typename T>
 struct set_next_same_type {
 
-	set_next_same_type(T* obj) : object(obj), set(false){};
+	set_next_same_type(T* an_object) : object(an_object), set(false){};
 
 	template<typename V>
 	void operator()(V& v,typename boost::enable_if<boost::is_same<V,T* > >::type* dummy = 0) const {
@@ -38,6 +56,10 @@ struct set_next_same_type {
 		//empty
 	}
 
+	bool has_succeeded() {
+		return set;
+	}
+
 private:
 	mutable T* object;
 	mutable bool set;
@@ -46,7 +68,7 @@ private:
 template<typename T>
 struct set_nth_same_type {
 
-	set_nth_same_type(T* obj,int at) : object(obj), counter(at){};
+	set_nth_same_type(T* an_object,int at) : object(an_object), counter(at){};
 
 	template<typename V>
 	void operator()(V& v,typename boost::enable_if<boost::is_same<V,T* > >::type* dummy = 0) const {
@@ -57,6 +79,10 @@ struct set_nth_same_type {
 	template<typename V>
 	void operator()(V& v,typename boost::disable_if<boost::is_same<V,T* > >::type* dummy = 0) const {
 		//empty
+	}
+
+	bool has_succeeded() {
+		return counter == -1;
 	}
 
 private:
