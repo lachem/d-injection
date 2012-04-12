115fcad6e78a9ac51b0d98f196006b3f2bb698ec
sal-vage <sal-vage@64c13286-791c-4059-bc77-4e3ab86eb2f2>
2012-04-12 18:51:15 +0000

    REFERENCE   : Development STABLE      : Yes TESTED      : Yes DESCRIPTION : Added noncopyable and nonallocatable.

---
+++ trunk/di/detail/utility.hpp
@@ -0,0 +1,27 @@
+//          Copyright Adam Lach 2012
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef DI_UTILITY_HPP
+#define DI_UTILITY_HPP
+
+namespace di {
+namespace detail {
+
+class noncopyable {
+	noncopyable(const noncopyable&);
+	noncopyable& operator=(const noncopyable&);
+public:
+	noncopyable() {}
+};
+
+class nonallocatable {
+	void* operator new(size_t);
+	void operator delete(void*);
+};
+
+} // namespace detail
+} // namespace di
+
+#endif //DI_UTILITY_HPP
