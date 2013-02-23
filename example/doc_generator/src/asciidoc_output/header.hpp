0bdcf7f570bf44ba1534c7d46a050913d506c16e
sal-vage <sal-vage@64c13286-791c-4059-bc77-4e3ab86eb2f2>
2013-02-23 17:34:33 +0000

    REFERENCE   : Development STABLE      : Yes TESTED      : Yes DESCRIPTION : - Updated file headers

--- example/doc_generator/src/asciidoc_output/header.hpp
+++ example/doc_generator/src/asciidoc_output/header.hpp
@@ -1,4 +1,4 @@
-//          Copyright Adam Lach 2012
+//          Copyright Adam Lach 2013
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
